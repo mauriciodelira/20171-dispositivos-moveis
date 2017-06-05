ca9b7afc572910be8fb338f287c1944a75bbd193
Mauricio de Lira <mauriciodlira@gmail.com>
2017-06-05 10:32:50 -0300

    Implementação de funcionalidades

--- android/ServicosExternos/app/src/main/java/com/mauricio/ifpb/pdm/servicosexternos/MainActivity.java
+++ android/ServicosExternos/app/src/main/java/com/mauricio/ifpb/pdm/servicosexternos/MainActivity.java
@@ -1,12 +1,16 @@
 package com.mauricio.ifpb.pdm.servicosexternos;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button btBrowser, btLigar, btDiscar, btEmail, btSMS, btCompartilharTexto, btPonto, btRota, btYoutube, btFoto;
@@ -117,34 +121,60 @@ public class MainActivity extends AppCompatActivity {
 
             if(v.equals(MainActivity.this.btDiscar)){
                 Log.i("CLICK-BOTAO","onClick - Discar");
+                Uri uri = Uri.parse("tel:83999184287");
+                Intent it = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(it);
 
             }
             if(v.equals(MainActivity.this.btLigar)){
                 Log.i("CLICK-BOTAO","onClick - Ligar");
+                Uri uri = Uri.parse("tel:83999184287");
+                Intent it = new Intent(Intent.ACTION_CALL, uri);
+                int permission = ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE);
+                if(permission == PackageManager.PERMISSION_GRANTED){
+                    startActivity(it);
+                }else
+                    Toast.makeText(MainActivity.this.getApplicationContext(), "Permissão negada.\nVerifique suas configurações", Toast.LENGTH_LONG).show();
 
             }
             if(v.equals(MainActivity.this.btEmail)){
                 Log.i("CLICK-BOTAO","onClick - Enviar Email");
-
+                Uri uri = Uri.parse("mailto:mauriciodlira@gmail.com");
+                Intent it = new Intent(Intent.ACTION_SENDTO, uri);
+                it.putExtra(Intent.EXTRA_SUBJECT, "Do que você quer falar?");
+                it.putExtra(Intent.EXTRA_TEXT, "Me diga que te ouço");
+                startActivity(Intent.createChooser(it, "Enviar email"));
             }
             if(v.equals(MainActivity.this.btSMS)){
                 Log.i("CLICK-BOTAO","onClick - Enviar SMS");
-
+                Intent it = new Intent(Intent.ACTION_SENDTO, Uri.parse("sms:83999184287"));
+                it.putExtra("sms_body", "Quer falar comigo?");
+                startActivity(it);
             }
             if(v.equals(MainActivity.this.btCompartilharTexto)){
                 Log.i("CLICK-BOTAO","onClick - Compartilhar");
+                Intent it = new Intent(.ACTION_SEND);
+                // Intent.setType = mime tipes.
+                it.setType("text/plain");
+                it.putExtra(Intent.EXTRA_TEXT, "Você é lindx de todo o coração do mundo. <3");
+                startActivity(Intent.createChooser(it, "Compartilhando seu texto"));
 
             }
             if(v.equals(MainActivity.this.btPonto)){
                 Log.i("CLICK-BOTAO","onClick - Ponto no mapa");
-
+                Uri uri = Uri.parse("gep:-8.0000000, -35.0000000");
+                Intent it = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(it);
             }
             if(v.equals(MainActivity.this.btRota)){
                 Log.i("CLICK-BOTAO","onClick - Rota no mapa");
-
+                String origem = "-7.0000394,-63333332"
+                String destino = "-1.0000394,-53333332"
             }
+
             if(v.equals(MainActivity.this.btYoutube)){
                 Log.i("CLICK-BOTAO","onClick - Youtube");
+                Uri uri = Uri.parse("bmvd")
 
             }
             else{
